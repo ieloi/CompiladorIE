22ab2422da0523667f4e47d114cea9a35af9ae1a
Ícaro Matheus Eloi do Nascimento <42556420+ieloi@users.noreply.github.com>
2019-05-29 21:22:48 -0300

    Inicio do Analisador Sintatico
    iniciada a produção do analisador sintático, com reconhecimento do inicio da escrita de programa e fim do mesmo juntamente com uma verificação básica de tipos de identificadores

--- Principal.cpp
+++ Principal.cpp
@@ -1,36 +1,114 @@
 #include "funcoes_e_variaveis.h"
 #include "lexico.h"
 
-int main() {
-	setlocale(LC_ALL, "Portuguese");
+void busca_token(ifstream& arq)
+{
+	while(!arq.eof())
+	{
+		if(erro == true)
+		{
+			break;
+		}
+		else
+		{
+			if(tk_encontrado == true)
+			{
+				verificaEstadoToken(estado);
+				automato(c);
+				tk_encontrado = false;
+				break;
+			}
+			arq.get(c);
+			automato(c);
+		}
+	}
+}
 
+void declara_tipo(ifstream& arq){
+	busca_token(arq);
+	do
+	{
+		if(novo_token.tipo_token_formado != "identificador")
+		{
+			msg = " identificador esperado ";
+		}
+		busca_token(arq);
+		if(novo_token.token_formado != "=")
+		{
+			msg = " = esperado ";
+		}
+		busca_token(arq);
+		if(novo_token.tipo_token_formado != "identificador")
+		{
+			msg = " identificador esperado ";
+		}
+		busca_token(arq);
+		if(novo_token.token_formado != ";")
+		{
+			msg = " ; esperado ";
+		}
+	} while(novo_token.tipo_token_formado == "identificador");
+}
+
+void bloco(ifstream& arq){
+	if(novo_token.token_formado == "tipo")
+	{
+		declara_tipo(arq);
+	}
+}
+
+void sintatico(ifstream& arq) {
+	
+	busca_token(arq);
+	if(novo_token.token_formado != "programa")
+	{
+		msg = "programa esperado na linha ";
+	}
+	
+	busca_token(arq);
+	if(novo_token.tipo_token_formado != "identificador")
+	{
+		msg = "identificador esperado na linha ";
+	}
+	
+	busca_token(arq);
+	if(novo_token.token_formado != ";")
+	{
+		msg = "; esperado na linha ";
+	}
+	
+	busca_token(arq);
+	bloco(arq);
+	
+	busca_token(arq);
+	if(novo_token.token_formado != ".")
+	{
+		msg = ". esperado na linha ";
+	}
+}
+
+int main()
+{
 	ifstream txtFile;
 
 	txtFile.open("teste.txt");
 
-	if(!txtFile) {
+	if(!txtFile)
+	{
 		cout << "erro de abertura do arquivo";
-	} else {
-		while(!txtFile.eof()) {
-			if(erro == true) {
-				break;
-			} else {
-				if(tk_encontrado == true) {
-					verificaEstadoToken(estado);
-					automato(c);
-					tk_encontrado = false;
-				} else {
-					txtFile.get(c);
-					automato(c);
-				}
-			}
-		}
+	}
+	else
+	{	
+		sintatico(txtFile);
 		txtFile.close();
 	}
 
-	if(erro == true) {
-		cout << endl << msg << countline++ << endl;
-	} else {
+	if(erro == true)
+	{
+		cout << endl << msg << endl;
+	}
+	else
+	{
 		cout << endl << msg << endl;
 	}
 
--- funcoes_e_variaveis.h
+++ funcoes_e_variaveis.h
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-struct info_token {
+struct info_token
+{
 	string token_formado;
 	int estado_token;
 	string tipo_token_formado;
@@ -27,31 +28,39 @@ string palavras_reservadas[21] = {"programa", "se", "entao", "senao", "enquanto"
 								  "funcao", "de", "para", "fim_do_se", "inicio", "fim"
 								 };
 
-int ehMaiusculoOuMinusculo(int c) {
+int ehMaiusculoOuMinusculo(int c)
+{
 	return isalpha(c);
 }
 
-int ehDigito(int c) {
+int ehDigito(int c)
+{
 	return isdigit(c);
 }
 
-int ehSimboloEspecial(int c) {
+int ehSimboloEspecial(int c)
+{
 	return ispunct(c);
 }
 
-int ehLetraOuDigito(int c) {
+int ehLetraOuDigito(int c)
+{
 	return isalnum(c);
 }
 
-int ehMinusculo(int c) {
+int ehMinusculo(int c)
+{
 	return islower(c);
 }
 
 // busca no array de palavras reservadas
-int eh_palavra_reservada(string tk_formado) {
+int eh_palavra_reservada(string tk_formado)
+{
 	int res = 0;
-	for(int i = 0; i < 21 ; i++) {
-		if(tk_formado == palavras_reservadas[i]) {
+	for(int i = 0; i < 21 ; i++)
+	{
+		if(tk_formado == palavras_reservadas[i])
+		{
 			res = 1;
 			break;
 		}
@@ -60,12 +69,13 @@ int eh_palavra_reservada(string tk_formado) {
 }
 
 // guarda as informações do token em uma estrutura
-info_token gera_token(string tipo) {
+info_token gera_token(string tipo)
+{
 	novo_token.token_formado = tk_formado;
 	novo_token.estado_token = estado;
 	novo_token.tipo_token_formado = tipo;
 
-	cout << " token formado: " << novo_token.token_formado << "\t" << "tipo: " << novo_token.tipo_token_formado << endl;
+	//cout << " token formado: " << novo_token.token_formado << "\t" << "tipo: " << novo_token.tipo_token_formado << endl;
 
 	estado = 0;
 	tk_formado = "";
@@ -74,31 +84,42 @@ info_token gera_token(string tipo) {
 }
 
 //Verifica o estado do token formado e define se ele é reconhecivel ou não
-void verificaEstadoToken(int es) {
-	switch(es) {
+void verificaEstadoToken(int es)
+{
+	switch(es)
+	{
 	case 1:
 		gera_token("identificador");
 		break;
 	case 4:
-		if(eh_palavra_reservada(tk_formado) == 1) {
+		if(eh_palavra_reservada(tk_formado) == 1)
+		{
 			gera_token("palavra reservada");
-		} else {
+		}
+		else
+		{
 			gera_token("identificador");
 		}
 		break;
 	case 7:
-		if(eh_palavra_reservada(tk_formado) == 1) {
+		if(eh_palavra_reservada(tk_formado) == 1)
+		{
 			gera_token("palavra reservada");
-		} else {
+		}
+		else
+		{
 			erro = true;
 			msg = "erro de compilacao na linha ";
 			break;
 		}
 		break;
 	case 10:
-		if(eh_palavra_reservada(tk_formado) == 1) {
+		if(eh_palavra_reservada(tk_formado) == 1)
+		{
 			gera_token("palavra reservada");
-		} else {
+		}
+		else
+		{
 			erro = true;
 			msg = "erro de compilacao na linha ";
 			break;
@@ -148,20 +169,32 @@ void verificaEstadoToken(int es) {
 }
 
 // funcao que delimita o fim de um token
-void encontrouFimToken(char c) {
-	if(c == ' ') {
+void encontrouFimToken(char c)
+{
+	if(c == ' ')
+	{
 		tk_encontrado = true;
-	} else if(c == '\t') {
+	}
+	else if(c == '\t')
+	{
 
-	} else if(c == '\n' && estado != 0) {
+	}
+	else if(c == '\n' && estado != 0)
+	{
 		tk_encontrado = true;
-	} else if(c == '\n' && estado == 0) {
+	}
+	else if(c == '\n' && estado == 0)
+	{
 		countline++;
-	} else if(c == '!' || c == '%' || c == '#' || c == '¨' || c == '&' || c == '?' || c == '°' || c == '|' || c == 'º' || c == '~' || c == '§' || c == '[' || c == ']' || c == 'ª') {
+	}
+	else if(c == '!' || c == '%' || c == '#' || c == '¨' || c == '&' || c == '?' || c == '°' || c == '|' || c == 'º' || c == '~' || c == '§' || c == '[' || c == ']' || c == 'ª')
+	{
 		verificaEstadoToken(estado);
 		erro = true;
 		msg = "erro de compilacao na linha ";
-	} else {
+	}
+	else
+	{
 		tk_encontrado = true;
 	}
 }
